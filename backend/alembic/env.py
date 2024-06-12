88f857f289e0ad22353901bd05bbd9fe4ce199db
Kailigithub <100wkl@163.com>
2024-06-12 17:19:07 +0800

    1.adjust database struct

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.database import Base
-from app.core.models import users, coin
+from app.core.models import users, coin, item
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
---
+++ backend/app/core/models/item.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, Float, VARCHAR, DateTime, Boolean
+from app.database import Base
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name=Column(VARCHAR(50))
+    describe = Column(VARCHAR(1500))
+    stock=Column(Integer)
+    create_time = Column(DateTime)
+    prince = Column(Float)
+    create_user=Column(Integer)
+    img_path=Column(VARCHAR(1000))
+
+class Order(Base):
+    __tablename__ = 'order'
+
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    user_id = Column(Integer)
+    itemid=Column(Integer)
+    quantity=Column(Integer)
+    order_type = Column(VARCHAR(200))
+    status=Column(VARCHAR(1))
+    toal_price=Column(Float)
+    address = Column(VARCHAR(1000))
+    create_time = Column(DateTime)
