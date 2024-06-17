b91728fff3990a4a71e3fd61e6f39fac98e7dfc3
liwei <liliweiwei2006@163.com>
2024-06-17 21:31:37 +0800

    seed password

--- backend/seed.py
+++ backend/seed.py
@@ -1,5 +1,8 @@
 import sqlite3
 import os
+from passlib.context import CryptContext
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+hashed_password = pwd_context.hash('zishu')
 def create_directory(directory):  
     if not os.path.exists(directory):  
         os.makedirs(directory)
@@ -7,22 +10,22 @@ create_directory("static")
 create_directory("static/profiles")
 con = sqlite3.connect('mydatabase.db')
 c = con.cursor()
+sql = '''
+INSERT INTO users (username, phone, role, email, password)
+    VALUES ('管理员', '15812345678', 'admin', 'example@example.cn', %s);
+'''% hashed_password
+sql1 = '''
+INSERT INTO users (username, phone, role, email, password)
+    VALUES ('黎伟', '15821123639', 'developer', 'omige@live.cn', %s);
+'''% hashed_password
 # sql = '''
-# INSERT INTO users (username, phone, role, email, password)
-#     VALUES ('管理员', '15812345678', 'admin', 'example@example.cn', '$2b$12$6S.75dg0aeWpnu8ZqJso/.fV6QSoe8Qz4WXpkHlTnAeHZq1b1x3n2');
+# INSERT INTO apply (user_id, repo, role, content, record_time)
+#     VALUES (2, 'zishu', 'developer', 'pr_3', '2024-06-01');
 # '''
 # sql1 = '''
-# INSERT INTO users (username, phone, role, email, password)
-#     VALUES ('黎伟', '15821123639', 'developer', 'omige@live.cn', '$2b$12$6S.75dg0aeWpnu8ZqJso/.fV6QSoe8Qz4WXpkHlTnAeHZq1b1x3n2');
+# INSERT INTO apply (user_id, repo, role, content, record_time)
+#     VALUES (2, 'zishu', 'developer', 'issue_5', '2024-06-02');
 # '''
-sql = '''
-INSERT INTO apply (user_id, repo, role, content, record_time)
-    VALUES (2, 'zishu', 'developer', 'pr_3', '2024-06-01');
-'''
-sql1 = '''
-INSERT INTO apply (user_id, repo, role, content, record_time)
-    VALUES (2, 'zishu', 'developer', 'issue_5', '2024-06-02');
-'''
 c.execute(sql)
 c.execute(sql1)
 con.commit()
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -5,6 +5,10 @@
 
 <template>
   <div>hello!</div>
+  <div><el-link type="primary" href="/coin/apply" target="_blank">鲸币申请</el-link></div>
+  <div><el-link type="primary" href="/coin/supervise" target="_blank">鲸币审批</el-link></div>
+  <div><el-link type="primary" href="/coin/consume" target="_blank">鲸币消费</el-link></div>
+  <div><el-link type="primary" href="/coin/bill" target="_blank">鲸币账单</el-link></div>
 </template>
 
 <style lang="scss" scoped>
