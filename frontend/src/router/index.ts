fffa1382618e2dfc2ed7662f4f00c7b4d80c7361
高增玉 <gaozengyu@cfhuoyun.com>
2024-06-19 13:59:06 +0800

    路由修改

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,96 +1,112 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
- 
+
 // 路由类型:RouteRecordRaw
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    // 命名
-    name: "index",
-    component: () => import("../components/Layout.vue"),
-    children: [
-        {
-            path: "/",
-            // 命名
-            name: "Home",
-            component: () => import("../views/Home.vue"),
-        },
-        {
-          path: "/user",
-          // 命名
-          name: "User",
-          component: () => import("../views/user/Center.vue"),
-        },
-        {
-        path: "/user/registers",
-          // 命名
-          name: "Register",
-          component: () => import("../views/user/Registers.vue"),
-        },
-        {
-          path: "/user/changepass",
-          // 命名
-          name: "Changepass",
-          component: () => import("../views/user/Changepass.vue"),
-        },
-        {
-          path: "/user/resetpass",
-          // 命名
-          name: "Resetpass",
-          component: () => import("../views/user/Resetpass.vue"),
-        },
-        {
-          path: "/user/editprofile",
-            // 命名
-            name: "Editprofile",
-            component: () => import("../views/user/Editprofile.vue"),
-        },
-        {
-          path: "/user/profile/:id",
-            // 命名
-            name: "Profile",
-            component: () => import("../views/user/Profile.vue"),
-        },
-        {
-          path: "/user/all",
-          // 命名
-          name: "All",
-          component: () => import("../views/user/All.vue"),
-        },
-        {
-          path: "/coin/apply",
-          // 命名
-          name: "Apply",
-          component: () => import("../views/coin/Apply.vue"),
-        },
-        {
-          path: "/coin/supervise",
-          // 命名
-          name: "Supervise",
-          component: () => import("../views/coin/Supervise.vue"),
-        },
-        {
-          path: "/coin/consume",
-          // 命名
-          name: "Consume",
-          component: () => import("../views/coin/Consume.vue"),
-        },
-        {
-          path: "/coin/bill",
-          // 命名
-          name: "Bill",
-          component: () => import("../views/coin/Bill.vue"),
-        },
-    ]
-  },
-  
-  
-  
+    {
+        path: "/",
+        // 命名
+        name: "index",
+        component: () => import("../components/Layout.vue"),
+        children: [
+            {
+                path: "/",
+                // 命名
+                name: "Home",
+                component: () => import("../views/Home.vue"),
+            },
+            {
+                path: "user",
+                // 命名
+                name: "User",
+                component: () => import("../views/user/Center.vue"),
+                beforeEnter: (to, from, next) => {
+                    // 这里是示例，实际情况下，你需要根据你的应用逻辑来判断用户是否已经登录
+                    const isAuthenticated = false;
+                    if (isAuthenticated) {
+                        next();
+                    } else {
+                        next('/login');
+                    }
+                },
+                children: [
+                    {
+                        path: "registers",
+                        // 命名
+                        name: "Register",
+                        component: () => import("../views/user/Registers.vue"),
+                    },
+                    {
+                        path: "changepass",
+                        // 命名
+                        name: "Changepass",
+                        component: () => import("../views/user/Changepass.vue"),
+                    },
+                    {
+                        path: "resetpass",
+                        // 命名
+                        name: "Resetpass",
+                        component: () => import("../views/user/Resetpass.vue"),
+                    },
+                    {
+                        path: "editprofile",
+                        // 命名
+                        name: "Editprofile",
+                        component: () => import("../views/user/Editprofile.vue"),
+                    },
+                    {
+                        path: "profile/:id",
+                        // 命名
+                        name: "Profile",
+                        component: () => import("../views/user/Profile.vue"),
+                    },
+                    {
+                        path: "all",
+                        // 命名
+                        name: "All",
+                        component: () => import("../views/user/All.vue"),
+                    },
+                ]
+            },
+            {
+                path: "coin",
+                // 命名
+                name: "Coin",
+                component: () => import("../views/coin/Apply.vue"),
+                children: [
+                    {
+                        path: "apply",
+                        // 命名
+                        name: "Apply",
+                        component: () => import("../views/coin/Apply.vue"),
+                    },
+                    {
+                        path: "supervise",
+                        // 命名
+                        name: "Supervise",
+                        component: () => import("../views/coin/Supervise.vue"),
+                    },
+                    {
+                        path: "consume",
+                        // 命名
+                        name: "Consume",
+                        component: () => import("../views/coin/Consume.vue"),
+                    },
+                    {
+                        path: "bill",
+                        // 命名
+                        name: "Bill",
+                        component: () => import("../views/coin/Bill.vue"),
+                    },
+                ]
+            },
+        ]
+    },
 ];
- 
+
 const router = createRouter({
-  // 路由模式
-  history: createWebHistory(),
-  routes,
+    // 路由模式
+    history: createWebHistory(),
+    routes,
 });
- 
+
 export default router;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,118 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
+import ViteCompression from 'vite-plugin-compression';
+import ViteComponents from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { EleAdminResolver, styleDeps } from 'ele-admin-pro/lib/utils/resolvers';
+import { DynamicAntdLess } from 'ele-admin-pro/lib/utils/dynamic-theme';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {                
-    host: '0.0.0.0'   
-  },
-})
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+  return {
+    resolve: {
+      alias: {
+        '@/': resolve(__dirname, 'src') + '/',
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+      }
+    },
+    plugins: [
+      vue(),
+      // 组件按需引入
+      ViteComponents({
+        dts: false,
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: 'less'
+          }),
+          EleAdminResolver({
+            importStyle: true
+          })
+        ]
+      }),
+      // gzip 压缩
+      ViteCompression({
+        disable: !isBuild,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz'
+      }),
+      // 兼容低版本浏览器
+      legacy({
+        targets: ['Chrome 63'],
+        modernPolyfills: true
+      })
+    ],
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          plugins: [new DynamicAntdLess()]
+        }
+      }
+    },
+    optimizeDeps: {
+      include: [
+        ...styleDeps,
+        'ant-design-vue',
+        'ant-design-vue/es',
+        'ele-admin-pro',
+        'ele-admin-pro/es',
+        '@ant-design/icons-vue',
+        'vuedraggable',
+        'dayjs',
+        'echarts/core',
+        'echarts/charts',
+        'echarts/renderers',
+        'echarts/components',
+        'vue-echarts',
+        'echarts-wordcloud',
+        'xlsx'
+      ]
+    },
+    server: {
+      proxy: {
+      //   // '/api': 'http://localhost:9100'
+      //   // '/api': 'https://app-gateway-dev.cfhy.work/cfhy-auth-center',
+      //   '/auth/': {
+      //     target: 'https://tech-dev.cfhy.work/auth',
+      //     // target: 'http://192.168.200.232:9100',
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/auth/, '')
+      //   },
+      //   '/api/': {
+      //     target: 'https://tech-dev.cfhy.work/api',
+      //     // target: 'http://192.168.200.232:9001',
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/api/, '')
+      //   },
+      //   '/sys/': {
+      //     target: 'https://tech-dev.cfhy.work/sys',
+      //     // target: 'http://192.168.200.232:9000',
+      //     changeOrigin: true,
+      //     rewrite: (path) => path.replace(/^\/sys/, '')
+      //   }
+      // }
+    },
+    build: {
+      target: 'chrome63',
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          external: [],
+          // 分包
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                  .toString()
+                  .split('node_modules/')[1]
+                  .split('/')[0]
+                  .toString();
+            }
+          }
+        }
+      }
+    }
+  };
+});
