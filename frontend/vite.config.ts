0308fc11a9815597de20fcb972dfb99253d8cf19
Kailigithub <100wkl@163.com>
2024-08-13 16:54:27 +0800

    Change Ports to 8008

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // 你的FastAPI后端地址
+        target: "http://localhost:8008", // 你的FastAPI后端地址
         changeOrigin: true,
         // 不需要 rewrite 配置，因为前后端路径一致
       },
