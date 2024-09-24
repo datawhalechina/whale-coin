5729aba71c9e528a2a6e2c9384710875c141768a
fengyunzaidushi <fengxiaoyang163@163.com>
2024-09-25 07:30:45 +0800

    add tailwind css

---
+++ frontend/src/components/CoinLayout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Header from './Header.vue'
+import NewHeader from './NewHeader.vue';
+import Footer from './Footer.vue'
+import Main from './Main.vue'
+</script>
+
+<template>
+  <el-container>
+      <el-header>
+        <Header />
+        <!-- <NewHeader /> -->
+      </el-header>
+
+      <el-main>
+       <Main />
+        <router-view></router-view>
+      </el-main>
+      <el-footer>
+        <Footer />
+      </el-footer>
+    </el-container>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,18 +1,30 @@
-<script setup lang="ts">
-
-
-</script>
-
 <template>
-  <div>hello!</div>
-  <div><el-link type="primary" href="/coin/apply" target="_blank">鲸币申请</el-link></div>
-  <div><el-link type="primary" href="/coin/supervise" target="_blank">鲸币审批</el-link></div>
-  <div><el-link type="primary" href="/coin/consume" target="_blank">鲸币消费</el-link></div>
-  <div><el-link type="primary" href="/coin/bill" target="_blank">鲸币账单</el-link></div>
-  <div><el-link type="primary" href="/item/item" target="_blank">鲸币商品</el-link></div>
-  <div><el-link type="primary" href="/item/order" target="_blank">鲸币订单</el-link></div>
+  <div class="">
+    <router-link
+      v-for="link in links"
+      :key="link.label"
+      :to="link.href"
+      class="inline-block px-4 py-2 mx-2 my-4 rounded-md bg-custom-color text-white"
+    >
+      {{ link.label }}
+    </router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { label: '鲸币申请', href: '/coin/apply' },
+        { label: '鲸币审批', href: '/coin/supervise' },
+        { label: '鲸币消费', href: '/coin/consume' },
+        { label: '鲸币账单', href: '/coin/bill' },
+        { label: '鲸币商品', href: '/item/item' },
+        { label: '鲸币订单', href: '/item/order' },
+      ],
+    };
+  },
+};
+</script>
