5673c1e8df3b44e01ab43feec9074d588bb1ac76
fengyunzaidushi <fengxiaoyang163@163.com>
2024-09-25 12:57:47 +0800

    恢复 register代码

--- frontend/src/views/user/Registers.vue
+++ frontend/src/views/user/Registers.vue
@@ -1,72 +1,76 @@
 <script setup lang="ts">
-import {fetchRigiAPI, handleRigiAPI} from '../../request/user/api'
-import { onMounted, ref, reactive } from 'vue'
-import { ElTable } from 'element-plus'
-document.title = "注册列表"
-let tableData:any = reactive([])
-const doRegister = async() => {
-    let res = await fetchRigiAPI()
-    tableData.push(...res)
-}
-onMounted(doRegister)
+import { fetchRigiAPI, handleRigiAPI } from "../../request/user/api";
+import { onMounted, ref, reactive } from "vue";
+import { ElTable } from "element-plus";
+document.title = "注册列表";
+let tableData: any = reactive([]);
+const doRegister = async () => {
+  let res = await fetchRigiAPI();
+  tableData.push(...res);
+};
+onMounted(doRegister);
 
-const currentRow = ref()
-const singleTableRef = ref<InstanceType<typeof ElTable>>()
+const currentRow = ref();
+const singleTableRef = ref<InstanceType<typeof ElTable>>();
 interface User {
-    id: number
-    name: string
-    email: string
-    phone: string
-    register_time: string
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  register_time: string;
 }
 
 const handleCurrentChange = (val: User | undefined) => {
-  currentRow.value = val
-}
+  currentRow.value = val;
+};
 
-const activate_user = async ()=> {
-    let msg = "确定要激活用户"+currentRow.value.name+"吗？"
-    if (confirm(msg)==true) {
-        console.log("激活")
-        let res = await handleRigiAPI({"action":"activate", "id":currentRow.value.id})
-        if (res.code == 200) {
-            let index = tableData.indexOf(currentRow.value);
-            if (index > -1) {
-                tableData.splice(index, 1);
-            }
-        }
-        
+const activate_user = async () => {
+  let msg = "确定要激活用户" + currentRow.value.name + "吗？";
+  if (confirm(msg) == true) {
+    console.log("激活");
+    let res = await handleRigiAPI({
+      action: "activate",
+      id: currentRow.value.id,
+    });
+    if (res.code == 200) {
+      let index = tableData.indexOf(currentRow.value);
+      if (index > -1) {
+        tableData.splice(index, 1);
+      }
     }
-}
+  }
+};
 
-const delete_user = async ()=> {
-    let msg = "确定要删除用户"+currentRow.value.name+"吗？"
-    if (confirm(msg)==true) {
-        console.log("删除")
-        let res = await handleRigiAPI({"action":"delete", "id":currentRow.value.id})
-        if (res.code == 200) {
-            let index = tableData.indexOf(currentRow.value);
-            if (index > -1) {
-                tableData.splice(index, 1);
-            }
-        }
+const delete_user = async () => {
+  let msg = "确定要删除用户" + currentRow.value.name + "吗？";
+  if (confirm(msg) == true) {
+    console.log("删除");
+    let res = await handleRigiAPI({
+      action: "delete",
+      id: currentRow.value.id,
+    });
+    if (res.code == 200) {
+      let index = tableData.indexOf(currentRow.value);
+      if (index > -1) {
+        tableData.splice(index, 1);
+      }
     }
-}
-
+  }
+};
 </script>
 
 <template>
-    <div v-if="currentRow">
+  <div v-if="currentRow">
     <el-button type="primary" @click="activate_user">激活</el-button>
     <el-button type="primary" @click="delete_user">删除</el-button>
-    </div>
-  <el-table 
-  ref="singleTableRef"
-  :data="tableData" 
-  stripe 
-  highlight-current-row
-  style="width: 100%"
-  @current-change="handleCurrentChange"
+  </div>
+  <el-table
+    ref="singleTableRef"
+    :data="tableData"
+    stripe
+    highlight-current-row
+    style="width: 100%"
+    @current-change="handleCurrentChange"
   >
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="email" label="Email" />
@@ -75,5 +79,4 @@ const delete_user = async ()=> {
   </el-table>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
