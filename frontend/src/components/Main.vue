0293a9f6e7b2664e15b63af9d4884e91687c9ea2
fengyunzaidushi <fengxiaoyang163@163.com>
2024-09-25 08:00:43 +0800

    add css

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,18 +1,37 @@
-<script setup lang="ts">  
-import { useLoginStore } from "../store";  
-const loginstate = useLoginStore();  
-const userid = loginstate.id  
-</script>  
+<script setup lang="ts">
+import { computed } from "vue";
+import { useLoginStore } from "../store";
 
-<template>  
-  <div>hello!</div>  
-  <div><router-link to="/coin/apply" target="_blank">鲸币申请</router-link></div>  
-  <div v-if="userid < 4"><router-link to="/coin/supervise" target="_blank">鲸币审批</router-link></div>  
-  <div><router-link to="/coin/consume" target="_blank">鲸币消费</router-link></div>  
-  <div><router-link to="/coin/bill" target="_blank">鲸币账单</router-link></div>  
-  <div><router-link to="/item/item" target="_blank">鲸币商品</router-link></div>  
-  <div><router-link to="/item/order" target="_blank">鲸币订单</router-link></div>  
-</template>  
+const loginstate = useLoginStore();
+const userId = loginstate.id;
 
-<style lang="scss" scoped>  
+// 定义链接并根据 userId 进行过滤
+const links = computed(() =>
+  [
+    { label: "鲸币申请", href: "/coin/apply" },
+    { label: "鲸币审批", href: "/coin/supervise", visible: userId < 4 },
+    { label: "鲸币消费", href: "/coin/consume" },
+    { label: "鲸币账单", href: "/coin/bill" },
+    { label: "鲸币商品", href: "/item/item" },
+    { label: "鲸币订单", href: "/item/order" },
+  ].filter((link) => link.visible !== false)
+); // 过滤不可见的链接
+</script>
+
+<template>
+  <div>
+    <router-link
+      v-for="link in links"
+      :key="link.label"
+      :to="link.href"
+      class="inline-block px-4 py-2 mx-2 my-4 rounded-md bg-custom-color text-white"
+    >
+      {{ link.label }}
+    </router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+/* 你可以在这里定义你的样式 */
 </style>
