e061b4c4355c23364ad5d3421c77692ba0cde2af
fengyunzaidushi <fengxiaoyang163@163.com>
2024-10-16 15:20:22 +0800

    rebuild dist

---
+++ frontend/dist/assets/Home-DcDsoo7-.js
@@ -0,0 +1,1 @@
+import{H as o}from"./HomeNavbar-C9rTEU-k.js";import{m as e,p as t,M as r}from"./vue-DRvwLzXx.js";import"./index-DW3BOA4A.js";import"./index-D85MJpsm.js";import"./crypto-BjADBdty.js";const f=e({__name:"Home",setup(m){return document.title="鲸币系统",(a,p)=>(t(),r(o))}});export{f as default};
--- frontend/dist/assets/index-kI_rcZ0B.js
+++
@@ -1,1 +0,0 @@
-import{aC as e}from"./vue-DRvwLzXx.js";const r=e("login",{state:()=>({id:0,logined:!1,dialogFormVisible:!1,name:"",atoken:"",rtoken:"",iframeurl:"http://christarter.com:8008"}),persist:{enabled:!0},getters:{},actions:{}});export{r as u};
